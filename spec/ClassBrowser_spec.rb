d491ff87c63c3e02b36d8f1c3c76aa2cf44a23b4
Jeff Rafter <jeffrafter@gmail.com>
2014-12-15 16:34:04 -0800

    Add an encoding

--- spec/ClassBrowser_spec.rb
+++ spec/ClassBrowser_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
@@ -5,7 +6,7 @@ require_relative '../lib/HierarchyWriter'
 require_relative '../lib/ClassBrowser'
 
 # sample classes used in tests below
-class Foo < Object;	end
+class Foo < Object; end
 class Bar < Foo; end
 
 class Parent < Object; end
@@ -15,34 +16,34 @@ class Grandchild < Sister; end
 
 describe HierarchyWriter do
 
-	before do
-		class Foo
-			attr_reader :name
-			attr_reader :descendants
+  before do
+    class Foo
+      attr_reader :name
+      attr_reader :descendants
 
-			def initialize name, descendants
-				@name = name
-				@descendants = descendants
-			end
-		end
+      def initialize name, descendants
+        @name = name
+        @descendants = descendants
+      end
+    end
 
-		@tree = Foo.new("Plant", [
-			Foo.new("Tree",	[
-				Foo.new("Pine", nil), 
-				Foo.new("Fir", nil)
-			]),
-			Foo.new("Flower", [
-				Foo.new("Rose", nil), 
-				Foo.new("Carnation", nil)
-			])
-		]);
-	end
+    @tree = Foo.new("Plant", [
+      Foo.new("Tree", [
+        Foo.new("Pine", nil),
+        Foo.new("Fir", nil)
+      ]),
+      Foo.new("Flower", [
+        Foo.new("Rose", nil),
+        Foo.new("Carnation", nil)
+      ])
+    ]);
+  end
 
-	context "::dump_descendants_of" do
+  context "::dump_descendants_of" do
 
-		it "can write out a simple Hierarchy of 'node' objects that respond to #name and #descendants" do
+    it "can write out a simple Hierarchy of 'node' objects that respond to #name and #descendants" do
 
-		expect { HierarchyWriter::dump_descendants_of(@tree) }.to output(
+    expect { HierarchyWriter::dump_descendants_of(@tree) }.to output(
 "○ Plant
 ├─○ Tree
 │ ├─○ Pine
@@ -51,26 +52,26 @@ describe HierarchyWriter do
   ├─○ Rose
   └─○ Carnation
 ").to_stdout
-		end
-	end
+    end
+  end
 
-	context "::dump_ancestors_of" do
+  context "::dump_ancestors_of" do
 
-		it "can write out the ancestors of 'node' objects that respond to #ancestors" do
-			expect { HierarchyWriter::dump_ancestors_of(ClassNode.new(Exception)) }.to output(
+    it "can write out the ancestors of 'node' objects that respond to #ancestors" do
+      expect { HierarchyWriter::dump_ancestors_of(ClassNode.new(Exception)) }.to output(
 "○ BasicObject
 └─○ Object
 ").to_stdout
-		end
+    end
 
-	end
+  end
 
-	context "::dump_hierarchy_of" do
+  context "::dump_hierarchy_of" do
 
-		it "can write out the hierarchy of 'node' objects" do
-			node = ClassNode.new Parent
+    it "can write out the hierarchy of 'node' objects" do
+      node = ClassNode.new Parent
 
-			expect { HierarchyWriter::dump_hierarchy_of(node) }.to output(
+      expect { HierarchyWriter::dump_hierarchy_of(node) }.to output(
 "○ BasicObject
 └─○ Object
   └─○ Parent
@@ -78,28 +79,28 @@ describe HierarchyWriter do
     └─○ Sister
       └─○ Grandchild
 ").to_stdout
-		end
-	end
+    end
+  end
 
-	context "::dump_hierarchy_of called with :depth_immediate" do
+  context "::dump_hierarchy_of called with :depth_immediate" do
 
-		it "can write out the hierarchy of 'node' objects" do
-			node = ClassNode.new Parent
+    it "can write out the hierarchy of 'node' objects" do
+      node = ClassNode.new Parent
 
-			expect { HierarchyWriter::dump_hierarchy_of(node, :depth_immediate) }.to output(
+      expect { HierarchyWriter::dump_hierarchy_of(node, :depth_immediate) }.to output(
 "○ BasicObject
 └─○ Object
   └─○ Parent
     ├─○ Brother
     └─○ Sister
 ").to_stdout
-		end
-	end
+    end
+  end
 
-	it "main runs with argument 'Parent'" do
-    	ARGV.clear
-    	ARGV << "Parent"
-    	expect { main }.to output(
+  it "main runs with argument 'Parent'" do
+      ARGV.clear
+      ARGV << "Parent"
+      expect { main }.to output(
 "○ BasicObject
 └─○ Object
   └─○ Parent
@@ -112,107 +113,107 @@ end
 
 describe ClassNode do
 
-	before do
-		@node = ClassNode.new Foo
-	end
+  before do
+    @node = ClassNode.new Foo
+  end
 
-	it "can return the name of the class" do
-		expect(@node.name).to eq("Foo")
-	end
+  it "can return the name of the class" do
+    expect(@node.name).to eq("Foo")
+  end
 
-	it "can return the ancestors of the class" do
-		expect(@node.ancestors).to contain_exactly(ClassNode.new(BasicObject), ClassNode.new(Object))
-	end
+  it "can return the ancestors of the class" do
+    expect(@node.ancestors).to contain_exactly(ClassNode.new(BasicObject), ClassNode.new(Object))
+  end
 
-	it "can return the descendants of the class" do
-		expect(@node.descendants).to eq([ClassNode.new(Bar)])
-	end
+  it "can return the descendants of the class" do
+    expect(@node.descendants).to eq([ClassNode.new(Bar)])
+  end
 
 end
 
 describe ClassBrowser do
 
-	before do
-		@browser = ClassBrowser.new 
-	end
+  before do
+    @browser = ClassBrowser.new
+  end
 
-	it "can parse the '-di' argument" do
-		@browser.parse_arguments ["-di"]
-		expect(@browser.depth).to eq(:depth_immediate)
-	end
+  it "can parse the '-di' argument" do
+    @browser.parse_arguments ["-di"]
+    expect(@browser.depth).to eq(:depth_immediate)
+  end
 
-	it "can parse the '-da' argument" do
-		@browser.parse_arguments ["-da"]
-		expect(@browser.depth).to eq(:depth_all)
-	end
+  it "can parse the '-da' argument" do
+    @browser.parse_arguments ["-da"]
+    expect(@browser.depth).to eq(:depth_all)
+  end
 
-	it "can parse the '-dn' argument" do
-		@browser.parse_arguments ["-dn"]
-		expect(@browser.depth).to eq(:depth_none)
-	end
+  it "can parse the '-dn' argument" do
+    @browser.parse_arguments ["-dn"]
+    expect(@browser.depth).to eq(:depth_none)
+  end
 
-	it "can parse the '-m' argument" do
-		@browser.parse_arguments ["-m"]
-		expect(@browser.show_modules).to eq(true)
-	end
+  it "can parse the '-m' argument" do
+    @browser.parse_arguments ["-m"]
+    expect(@browser.show_modules).to eq(true)
+  end
 
-	it "can parse the '-mn' argument" do
-		@browser.parse_arguments ["-mn"]
-		expect(@browser.show_methods).to eq(:methods_none)
-	end
+  it "can parse the '-mn' argument" do
+    @browser.parse_arguments ["-mn"]
+    expect(@browser.show_methods).to eq(:methods_none)
+  end
 
-	it "can parse the '-ma' argument" do
-		@browser.parse_arguments ["-ma"]
-		expect(@browser.show_methods).to eq(:methods_all)
-	end
+  it "can parse the '-ma' argument" do
+    @browser.parse_arguments ["-ma"]
+    expect(@browser.show_methods).to eq(:methods_all)
+  end
 
-	it "can parse the '-mi' argument" do
-		@browser.parse_arguments ["-mi"]
-		expect(@browser.show_methods).to eq(:methods_instance)
-	end
+  it "can parse the '-mi' argument" do
+    @browser.parse_arguments ["-mi"]
+    expect(@browser.show_methods).to eq(:methods_instance)
+  end
 
-	it "can parse the '-mc' argument" do
-		@browser.parse_arguments ["-mc"]
-		expect(@browser.show_methods).to eq(:methods_class)
-	end
+  it "can parse the '-mc' argument" do
+    @browser.parse_arguments ["-mc"]
+    expect(@browser.show_methods).to eq(:methods_class)
+  end
 
-	it "can parse a class name argument" do
-		@browser.parse_arguments ["-foo", "Array", "-bar"]
-		expect(@browser.class_root_node.klass.name).to eq("Array")
-	end
+  it "can parse a class name argument" do
+    @browser.parse_arguments ["-foo", "Array", "-bar"]
+    expect(@browser.class_root_node.klass.name).to eq("Array")
+  end
 
-	it "a bogus class name argument returns nil" do
-		@browser.parse_arguments ["-foo", "Jabberwocky", "-bar"]
-		expect(@browser.class_root_node.klass).to eq(nil)
-	end
+  it "a bogus class name argument returns nil" do
+    @browser.parse_arguments ["-foo", "Jabberwocky", "-bar"]
+    expect(@browser.class_root_node.klass).to eq(nil)
+  end
 
-	it "dumps a list of a class's methods" do
-		@browser.parse_arguments ["Array", "-ma"]
-		expect{ @browser.dump }.to output(/#pretty_print_cycle/).to_stdout
-	end
+  it "dumps a list of a class's methods" do
+    @browser.parse_arguments ["Array", "-ma"]
+    expect{ @browser.dump }.to output(/#pretty_print_cycle/).to_stdout
+  end
 
-	it "dumps a list of a class's included modules" do
-		@browser.parse_arguments ["Array", "-m"]
-		expect{ @browser.dump }.to output(/[Enumerable]/).to_stdout
-	end
+  it "dumps a list of a class's included modules" do
+    @browser.parse_arguments ["Array", "-m"]
+    expect{ @browser.dump }.to output(/[Enumerable]/).to_stdout
+  end
 
-	it "dumps help" do
-		@browser.parse_arguments ["-h"]
-		expect{ @browser.dump }.to output(/ClassBrowser with no arguments enters interactive mode/).to_stdout
-	end
+  it "dumps help" do
+    @browser.parse_arguments ["-h"]
+    expect{ @browser.dump }.to output(/ClassBrowser with no arguments enters interactive mode/).to_stdout
+  end
 
 end
 
 describe "The ClassBrowser can be invoked from the command line" do
 
-	before do
-		@browser = ClassBrowser.new
-	end
+  before do
+    @browser = ClassBrowser.new
+  end
 
     it "main runs with argument 'String' and dumps the String hierarchy" do
-    	ARGV.clear
-    	ARGV << "String"
-      	expect { main }.to output(
+      ARGV.clear
+      ARGV << "String"
+        expect { main }.to output(
 "○ BasicObject
 └─○ Object
   └─○ String
@@ -220,8 +221,8 @@ describe "The ClassBrowser can be invoked from the command line" do
     end
 
     it "main runs in interactive mode with user input 'Array' then dumps the Array hierarchy then quits" do
-		expect(@browser).to receive(:gets).twice.and_return("Array", "\n")
-		expect { @browser.interactive }.to output(
+    expect(@browser).to receive(:gets).twice.and_return("Array", "\n")
+    expect { @browser.interactive }.to output(
 /○ BasicObject
 └─○ Object
   └─○ Array
