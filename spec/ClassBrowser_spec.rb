1994718c4dc40c26dcadd67f91471423236a6a4e
Jeff Rafter <jeffrafter@gmail.com>
2014-12-15 16:58:48 -0800

    Fix requires

--- spec/ClassBrowser_spec.rb
+++ spec/ClassBrowser_spec.rb
@@ -3,7 +3,7 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require_relative '../lib/HierarchyWriter'
-require_relative '../lib/ClassBrowser'
+require_relative '../lib/class_browser'
 
 # sample classes used in tests below
 class Foo < Object; end
